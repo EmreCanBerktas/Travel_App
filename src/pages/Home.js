47c0d8dc1acc33fa4477ef55107e22ae60252719
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2021-12-29 23:20:20 +0300

    Home text changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,14 +11,14 @@ function Home() {
         <img src={travel} width="400" height="150" alt="Travel"></img>
         <h2>Gitmek istediğin yere güvenli ve ekonomik bir şekilde git</h2>
         <img src={target} width="400" height="150" alt="Target"></img>
-        <h2>Gitmek istediğin yere güvenli ve ekonomik bir şekilde git</h2>
+        <h2>Hedefine zamanında ulaş hiç vakit kaybetme</h2>
         <img
           src={travelTogether}
           width="400"
           height="250"
           alt="Travel Together"
         ></img>
-        <h2>Gitmek istediğin yere güvenli ve ekonomik bir şekilde git</h2>
+        <h2>Yeni insanlar tanı yeni yüzler keşfet</h2>
       </div>
     </div>
   );
